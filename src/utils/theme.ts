deeb95894d9ae22a82bee8bd80d92edd6551db28
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-11-19 17:24:41 +0200

    init

---
+++ src/utils/theme.ts
@@ -0,0 +1,26 @@
+const theme: object = {
+  colors: {
+    accent: {
+      primary: "#330031",
+      secondery: "FFCCFD",
+    },
+    background: {
+      primary: "background-color: #FFFFFF;",
+      secondery: "background-color: #330031;",
+    },
+    text: {
+      primary: "color: #FFFFFF;",
+      secondery: "color: #000000;",
+    },
+    icons: {
+      primary: "#FF66FA",
+    },
+  },
+  borders: {
+    links: {
+      primary: "border: solid 1px #000000;",
+    },
+  },
+};
+
+export { theme };
