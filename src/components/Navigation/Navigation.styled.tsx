f05f2e9e7d382d3965735156d8bd93a7d8ad2c47
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-11-21 19:29:42 +0200

    Styled navigation

--- src/components/Navigation/Navigation.styled.tsx
+++ src/components/Navigation/Navigation.styled.tsx
@@ -2,4 +2,10 @@ import styled from "styled-components";
 
 interface IStyledNavigation {}
 
-export const StyledNavigation = styled("nav")``;
+export const StyledNavigation = styled("nav")`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  flex-grow: 0;
+  gap: 70px;
+`;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,5 +3,12 @@ import { StyledNavigation } from "./Navigation.styled";
 
 interface INavigation {}
 export const Navigation: React.FC<INavigation> = () => {
-  return <StyledNavigation></StyledNavigation>;
+  return (
+    <StyledNavigation>
+      <NavigationLink to="">Movies</NavigationLink>
+      <NavigationLink to="Series">Series</NavigationLink>
+      <NavigationLink to="Contact">Contact</NavigationLink>
+      <NavigationLink to="AboutUs">About Us</NavigationLink>
+    </StyledNavigation>
+  );
 };
