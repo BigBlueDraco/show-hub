0013f0b6b96d95d416fed561a37f2a4d0911d9fd
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-11-21 20:25:19 +0200

    header flex

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
 
 interface IStyledHeader {}
-export const StyledHeader = styled("header")<IStyledHeader>``;
+export const StyledHeader = styled("header")<IStyledHeader>`
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  padding: 0px;
+  gap: 114px;
+`;
