a08f89cae3863029aa237258b90cf0dec4c55fb7
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-12-02 15:12:42 +0200

    created class and featheTopRateMovie

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,10 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./utils/theme";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./utils/GlobalStyles";
-
+import { TMDBApi } from "./services/TMDBApi";
+const api = new TMDBApi();
+api.featheTopRateMovies();
+console.log(api.featheTopRateMovies());
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
---
+++ src/services/TMDBApi.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+export class TMDBApi {
+  private BASE_URL = "https://api.themoviedb.org/3/";
+  private API_KEY = "fd0a7baec7e1722e7549dfbeea65227f";
+
+  public async featheTopRateMovies() {
+    const res = await axios.get(`movie/top_rated`, {
+      baseURL: this.BASE_URL,
+      params: {
+        api_key: this.API_KEY,
+      },
+    });
+    console.log(res);
+    return await res;
+  }
+}
