15f09944ac40ec80b29a29e9694a528a533d3988
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-02-04 15:47:11 +0200

    ref pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,15 @@
-import { useEffect, useState } from "react";
-
-import { Header } from "./components/Header/Header";
-import { Hero } from "./components/Hero/Hero";
-import { Footer } from "./components/Footer/Footer";
-import { CarouselSection } from "./components/CarouselSection/CarouselSection";
-import { TMDBApi } from "./services/TMDBApi";
 import { Route, Routes } from "react-router-dom";
-import { ModalWindowWraper } from "./components/ModalWindowWraper/ModalWindowWraper";
+import { HomePage } from "./Pages/HomePage/HomePage";
+import { Layout } from "./Pages/Layout/Layout";
 
 function App() {
-  const [topMoviesAllTheTime, setTopMoviesAllTheTime] = useState();
-  const [topTVsAllTheTime, setTopTVsAllTheTime] = useState();
-  useEffect(() => {
-    const feath = async () => {
-      const api = new TMDBApi();
-      const topMovies = await api.featheTopRateMovies();
-      const topTVs = await api.featheTopRateTVs();
-      setTopMoviesAllTheTime(topMovies.results);
-      setTopTVsAllTheTime(topTVs.results);
-    };
-    feath();
-  }, []);
   return (
     <div>
       <Routes>
-        <Route
-          index
-          element={
-            <>
-              <Header></Header>
-              <Hero />
-              <CarouselSection
-                title="Top Movies"
-                elements={topMoviesAllTheTime}
-              />
-              <CarouselSection
-                title="Top TV shows"
-                elements={topTVsAllTheTime}
-                last
-              />
-              <Footer />
-            </>
-          }
-        ></Route>
-        <Route path="/:movieId" element={<div>movie page</div>}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path=":movieId" element={<div>movie page</div>} />
+        </Route>
       </Routes>
     </div>
   );
--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,3 +1,27 @@
+import { CarouselSection } from "../../components/CarouselSection/CarouselSection";
+import { ModalWindowWraper } from "../../components/ModalWindowWraper/ModalWindowWraper";
+import { TMDBApi } from "../../services/TMDBApi";
+import { Hero } from "../../components/Hero/Hero";
+import { useState, useEffect } from "react";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const [topMoviesAllTheTime, setTopMoviesAllTheTime] = useState();
+  const [topTVsAllTheTime, setTopTVsAllTheTime] = useState();
+  useEffect(() => {
+    const feath = async () => {
+      const api = new TMDBApi();
+      const topMovies = await api.featheTopRateMovies();
+      const topTVs = await api.featheTopRateTVs();
+      setTopMoviesAllTheTime(topMovies.results);
+      setTopTVsAllTheTime(topTVs.results);
+    };
+    feath();
+  }, []);
+  return (
+    <>
+      <Hero />
+      <CarouselSection title="Top Movies" elements={topMoviesAllTheTime} />
+      <CarouselSection title="Top TV shows" elements={topTVsAllTheTime} last />
+    </>
+  );
 };
